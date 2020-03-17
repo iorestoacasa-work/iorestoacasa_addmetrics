b9359a0e83be86ab64ced62fd3049a68dbaf7249
Luca Ferroni <luca@befair.it>
2020-03-17 16:19:08 +0100

    Reso idempotente, fixato /var/www nello script systemd

--- add-metrics.sh
+++ add-metrics.sh
@@ -3,22 +3,35 @@
 echo "Warning: replacing JVB_OPTS in videobridge config"
 sed -i.bak s/JVB_OPTS=\"\"/JVB_OPTS=\"--apis=rest,xmpp\"/g /etc/jitsi/videobridge/config
 
-echo "Adding export statistics to sip-communicator properties"
-cat << EOF >> /etc/jitsi/videobridge/sip-communicator.properties
+grep "org.jitsi.videobridge.ENABLE_STATISTICS=true"  /etc/jitsi/videobridge/sip-communicator.properties
+if [ ! "$?" -eq 0 ]; then
+    echo "Adding export statistics to sip-communicator properties"
+    cat << EOF >> /etc/jitsi/videobridge/sip-communicator.properties
 org.jitsi.videobridge.ENABLE_STATISTICS=true
 org.jitsi.videobridge.STATISTICS_TRANSPORT=muc,colibri
 org.jitsi.videobridge.STATISTICS_INTERVAL=5000
 EOF
+fi
 
 echo "Restarting the videobridge"
 systemctl restart jitsi-videobridge.service jicofo.service
 
-echo "Allowing firewall on port 8082"
-ufw allow in 8082/tcp
+iptables -L INPUT -n -v | grep 8080 | grep tcp
+if [ ! "$?" -eq 0 ]; then
+    echo "Denying firewall on port 8080"
+    iptables -I INPUT 1 -j DROP -p tcp --destination-port=8080
+    iptables -I INPUT 1 -i lo -j ACCEPT -p tcp --destination-port=8080
+fi
+iptables -L INPUT -n -v | grep 8082 | grep tcp
+if [ ! "$?" -eq 0 ]; then
+    echo "Allowing firewall on port 8082"
+    iptables -I INPUT 1 -j ACCEPT -p tcp --destination-port=8082
+fi
 
 echo "Adding systemd service"
-cp iorestoacasa_metrics.service /etc/systemd/system/
+sed 's@IORESTOACASA_PLACEHOLDER@$(pwd)/iorestoacasa_metrics.py@g' iorestoacasa_metrics.service > /etc/systemd/system/iorestoacasa_metrics.service
 
 echo "Starting to export metrics!"
-systemctl start iorestoacasa_metrics.service
 systemctl enable iorestoacasa_metrics.service
+systemctl daemon-reload
+systemctl start iorestoacasa_metrics.service
